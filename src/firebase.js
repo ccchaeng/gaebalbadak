2e37f649ca5f91d26cfdc8e32e42a7297a3673a2
ccchaeng <chaeng30415@naver.com>
2025-02-20 17:35:22 +0900

    Chore: firebase 연동
    - 패키지 매니저 수정
    
    - firebase 설정 파일 생성 (firebase.js)

---
+++ src/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBUPIAUFhEMpieQ1efiYjlH4097L7wtSIw",
+  authDomain: "gaebalbadak.firebaseapp.com",
+  projectId: "gaebalbadak",
+  storageBucket: "gaebalbadak.firebasestorage.app",
+  messagingSenderId: "643880875095",
+  appId: "1:643880875095:web:41f0c2f10d999034a65f12",
+  measurementId: "G-LS79ST4GQW"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
