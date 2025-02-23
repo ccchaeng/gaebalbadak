bbc53168b1848846dd3b6f67cb9d6d5723b7943a
cjh5779 <choijh5779@gmail.com>
2025-02-23 22:12:53 +0900

    컴포넌트화

--- src/components/common/Banner.jsx
+++ src/components/common/Banner.jsx
@@ -1,14 +1,13 @@
 import React from "react";
-import bannerImage from "../../assets/banner2.jpg";
 import styles from "./Banner.module.scss";
 
-function Banner() {
+function Banner({ image, title = "개발자들의 바닥까지 파헤치는 공간,", description = "세상을 움직이는 코드, 여기서 시작됩니다." }) {
   return (
-    <section className={styles.banner} style={{ backgroundImage: `url(${bannerImage})` }}>
+    <section className={styles.banner} style={{ backgroundImage: `url(${image})` }}>
       <div className={styles.bannerContentWrapper}>
         <div className={styles.bannerContent}>
-          <h1>개발자들의 바닥까지 파헤치는 공간,</h1>
-          <p>세상을 움직이는 코드, 여기서 시작됩니다.</p>
+          <h1>{title}</h1>
+          <p>{description}</p>
         </div>
       </div>
     </section>
