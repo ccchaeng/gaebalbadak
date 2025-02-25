51c8eb2132f979967aa10c0c57a3611e3c49cda6
cjh5779 <choijh5779@gmail.com>
2025-02-26 02:44:33 +0900

    페이지별로 위치 수정

--- src/pages/Apply.jsx
+++ src/pages/Apply.jsx
@@ -1,11 +1,26 @@
-import React from 'react'
+import React from 'react';
+import bannerImage from "../assets/banner3.jpg";
+import Banner from "../components/common/Banner";
+import SearchBar from "../components/common/SearchBar";
+import styles from "./Apply.module.scss";
 
 function Apply() {
   return (
-    <div>
-        신청할래
+    <div className={styles.homeContainer}>
+      {/* 🔥 배너 (검색창 오버레이 포함) */}
+      <div className={styles.bannerWrapper}>
+        <Banner 
+          image={bannerImage} 
+          title="신청할래?" 
+          description="세상을 움직이는 코드, 여기서 시작됩니다." 
+          className={styles.customBanner}
+        />
+        <div className={styles.searchBarOverlay}>
+          <SearchBar />
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Apply
+export default Apply;
--- src/pages/Collaboration.jsx
+++ src/pages/Collaboration.jsx
@@ -1,11 +1,27 @@
 import React from 'react'
+import bannerImage from "../assets/banner3.jpg";
+import Banner from "../components/common/Banner";
+import SearchBar from "../components/common/SearchBar";
+import styles from "./Collaboration.module.scss";
+
 
 function Collaboration() {
   return (
-    <div>
-        같이할래
+    <div className={styles.homeContainer}>
+      {/* 🔥 배너 (검색창 오버레이 포함) */}
+      <div className={styles.bannerWrapper}>
+        <Banner 
+          image={bannerImage} 
+          title="같이할래?" 
+          description="저희 프로젝트에 함께 참여해보시는 건 어떨까요?" 
+          className={styles.customBanner}
+        />
+        <div className={styles.searchBarOverlay}>
+          <SearchBar />
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
 export default Collaboration
