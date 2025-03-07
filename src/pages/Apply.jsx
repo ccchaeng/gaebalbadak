10dbd4fdaf388b8a498ef12a7aa6eca2ba9db588
cjh5779 <choijh5779@gmail.com>
2025-03-07 20:52:36 +0900

    수정

--- src/pages/Apply.jsx
+++ src/pages/Apply.jsx
@@ -1,26 +1,24 @@
-import React from 'react';
+import React from "react";
 import bannerImage from "../assets/banner3.jpg";
 import Banner from "../components/common/Banner";
 import SearchBar from "../components/common/SearchBar";
+
 import styles from "./Apply.module.scss";
-import Box from "../components/common/Box"; 
 
 function Apply() {
   return (
     <div className={styles.homeContainer}>
-      {/* 🔥 배너 (검색창 오버레이 포함) */}
       <div className={styles.bannerWrapper}>
         <Banner 
           image={bannerImage} 
           title="신청할래?" 
-          description="같이 도전하고 성장할 팀원을 찾아보세요!" 
+          description="대외활동, 공모전, 지원사업! 지금 바로 신청할래?" 
           className={styles.customBanner}
         />
         <div className={styles.searchBarOverlay}>
           <SearchBar />
         </div>
       </div>
-      <Box></Box>
     </div>
   );
 }
