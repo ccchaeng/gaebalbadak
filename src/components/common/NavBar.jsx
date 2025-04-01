a246e68dd55a138847e809d985354a11e11030d0
ccchaeng <chaeng30415@naver.com>
2025-04-01 18:17:37 +0900

    Feat: NavBar에서 프로필 이미지 클릭 시 프로필 페이지 연결 안 되는 문제 해결

--- src/components/common/NavBar.jsx
+++ src/components/common/NavBar.jsx
@@ -7,28 +7,29 @@ import default_profile from "/default_profile.png"
 // 컴포넌트
 import ProfileImage from "./ProfileImage";
 
-// Firebase 관련 기능
-import { auth, db } from "../../firebase";
-import { onAuthStateChanged, signOut } from "firebase/auth";
+// Firebase
+import { db } from "../../firebase";
 import { doc, getDoc } from "firebase/firestore";
 
+import useAuth from "../../hooks/useAuth";
+
 function NavBar({ className = "" }) {
-  const [user, setUser] = useState(null);
+  const { user } = useAuth(); // 로그인한 유저 정보
   const [profile, setProfile] = useState({ nickname: "", photoURL: "" });
 
-  // 로그인 상태 확인과 사용자 정보 불러오기
   useEffect(() => {
-    const unsubscribe = onAuthStateChanged(auth, async (currentUser) => {
-      if (currentUser) {
-        const docRef = doc(db, "users", currentUser.uid);
+    const fetchProfile = async () => {
+      if (user) {
+        const docRef = doc(db, "users", user.uid);
         const docSnap = await getDoc(docRef);
-        if (docSnap.exists()) setProfile(docSnap.data());
-        setUser(currentUser);
-      } else setUser(null);
-    });
+        if (docSnap.exists()) {
+          setProfile(docSnap.data());
+        }
+      }
+    };
 
-    return () => unsubscribe();
-  }, []);
+    fetchProfile();
+  }, [user]);
 
   return (
     <nav className={`${styles.navBar} ${className}`}>
@@ -37,21 +38,17 @@ function NavBar({ className = "" }) {
       <Link to="/apply">신청할래</Link>
 
       <div className={styles.authGroup}>
-        {/* 로그인 했을 경우 프로필만 보이게 */}
         {user ? (
-          <>
-            <div className={styles.profileContainer}>
-              <Link to="/profile">
-                <ProfileImage 
-                  photoURL={profile.photoURL}
-                  nickname={profile.nickname}
-                  className={styles.image}
-                />
-              </Link>
-            </div>
-          </>
+          <div className={styles.profileContainer}>
+            <Link to={`/profile/${user.uid}`}>
+              <ProfileImage
+                photoURL={profile.photoURL || default_profile}
+                nickname={profile.nickname}
+                className={styles.image}
+              />
+            </Link>
+          </div>
         ) : (
-          // 로그인 안 했을 경우 로그인, 회원가입 버튼 보이게
           <>
             <Link to="/login">Login</Link>
             <Link to="/signup">
